153c1a6e1cd0a0328e03e27dacdd451471adeed5
unknown <Ciprian Tarlev>
2018-12-08 13:57:52 +0200

    created a java project

---
+++ src/firstExercise.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class firstExercise {
+	public static void main (String[] args) {
+		int sum = 0;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Please enter a number: ");
+		
+		int n = scanner.nextInt();
+		
+		while (n != 0) {
+			sum = sum + n%10;
+			n = n/10;
+		}
+		System.out.println(sum);
+	}
+}
